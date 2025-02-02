7c6af9848301d25bd0d50b997f3a8fd86906c9b3
Pranav S <167344853+prxnxv02@users.noreply.github.com>
2025-02-02 14:05:32 +0530

    config.js

--- config.js
+++ config.js
@@ -1,22 +1,28 @@
-import { createChatBotMessage } from 'react-chatbot-kit';
-import Avatar from './components/Avatar';
-import StartBtn from './components/StartBtn';
-import StartSlow from './components/StartSlow';
-import data from './data';
-import DipslayImage from './components/DisplayImage';
-
+import { createChatBotMessage } from "react-chatbot-kit";
+import Avatar from "./components/Avatar";
+import StartBtn from "./components/StartBtn";
+import StartSlow from "./components/StartSlow";
+import data from "./data";
+import DisplayImage from "./components/DisplayImage";
+import ImageUpload from "./components/ImageUpload"; // Import ImageUpload component
 
 const config = {
-    botName: "AgeDrive Explorer",
-    initialMessages: [createChatBotMessage(`Welcome to RideAge Advisor! click the below "Let's Get Started" to continue"`, {
-        widget: "startBtn"
-    })],
+    botName: "Our chatbot",
+    initialMessages: [
+        createChatBotMessage(`Welcome to ABC Advisor! Click "Let's Get Started" to continue.`, {
+            widget: "startBtn"
+        }),
+        createChatBotMessage("You can also upload an image if needed.", {
+            widget: "imageUpload"
+        })
+    ],
     customComponents: {
         botAvatar: (props) => <Avatar {...props} />,
     },
     state: {
         checker: null,
         data,
+        userImage: null, // Store uploaded image
         userData: {
             name: "",
             age: 0,
@@ -39,9 +45,13 @@ const config = {
         },
         {
             widgetName: "finalImage",
-            widgetFunc: (props) => <DipslayImage {...props} />,
+            widgetFunc: (props) => <DisplayImage {...props} />,
         },
-    ]
+        {
+            widgetName: "imageUpload",
+            widgetFunc: (props) => <ImageUpload {...props} />, // Image upload widget
+        },
+    ],
 };
 
 export default config;
